c0ea6c24483a2ebc4f49e1c3e0eba958b1af6124
Celson Andrade <celson.bac@gmail.com>
2024-04-25 17:54:43 -0300

    Codigo melhorado

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,6 +1,5 @@
 package cliente;
 
-import java.io.PrintStream;
 import java.net.*;
 import java.util.Scanner;
 import org.json.JSONObject;
@@ -8,28 +7,84 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 public class Cliente {
+	
+	public static Socket clienteSocket;
 
 	
-	public static void main (String[] args) {
+	public static void main (String[] args) throws UnknownHostException, IOException {
+		clienteSocket = new Socket("teste",2299);
+		
+		int op=2;
+		
+		OutputStreamWriter out = new OutputStreamWriter(clienteSocket.getOutputStream(), StandardCharsets.UTF_8);
+		out.write(op);
+		
+		//Scanner leia = new Scanner(System.in);
+		
+		
+		//op = leia.nextInt();
+		switch(op) {
+		case 1: 
+			CadastrarUsuario();
+		break;
+		case 2:
+			RealizarLogin();
+		break;
+		}
+		
+			
+	}
+	
+	
+	public static void CadastrarUsuario() {
 		try 
 		{
 			JSONObject json = new JSONObject();
 			json.put("type", "CONNECT");
 			
-			Socket clienteSocket = new Socket("teste",8787);
 			
+			
+			Scanner leia = new Scanner(System.in);
 			try (OutputStreamWriter out = new OutputStreamWriter(
-					clienteSocket.getOutputStream(), StandardCharsets.UTF_8)) {
+				clienteSocket.getOutputStream(), StandardCharsets.UTF_8)) {
 				
-				 String myString = new JSONObject()
-				         .put("Email:", "Celsond@alunos.utfpr.edu.br").put("Senha:", "121212333").put("nome:", "Celson").toString();
-				 
-				 
-				 
-				 
+				System.out.println("Digite seu nome:");
+				String nome = leia.nextLine(); 
+				System.out.println("Digite sua senha:");
+				String senha= leia.nextLine();
+				System.out.println("Digite seu email:");
+				String email = leia.nextLine(); 
+				String myString = new JSONObject().put("Email:", email).put("Senha:", senha).put("nome:", nome).toString(); 
 			    out.write(myString);
 			}
 			
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex);
+				
+		}
+	}
+	
+	public static void RealizarLogin() {
+		try 
+		{
+			JSONObject json = new JSONObject();
+			json.put("type", "CONNECT");
+			
+			
+			
+			Scanner leia = new Scanner(System.in);
+			try (OutputStreamWriter out = new OutputStreamWriter(
+				clienteSocket.getOutputStream(), StandardCharsets.UTF_8)) {
+				
+				System.out.println("Digite seu email:");
+				String email = leia.nextLine();
+				System.out.println("Digite sua senha:");
+				String senha= leia.nextLine();
+				String myString = new JSONObject().put("Email:", email).put("Senha:", senha).toString(); 
+			    out.write(myString);
+			}
 			
 		}
 		catch(Exception ex)
--- src/servidor/Servidor.java
+++ src/servidor/Servidor.java
@@ -9,54 +9,59 @@ import java.util.Scanner;
 import dao.*;
 import java.util.*;
 import com.fasterxml.jackson.databind.ObjectMapper; 
-import com.fasterxml.jackson.core.type.TypeReference;  
-public class Servidor {
+import com.fasterxml.jackson.core.type.TypeReference; 
 
-	public static void main(String[] args){
+public class Servidor {
+	
+	public static ServerSocket servidorSocket;
+	
+	public static void main(String[] args) throws IOException{
+		System.out.println("programa 1: servidor");
 		
+		servidorSocket = new ServerSocket(2299,5);
+		Socket ss=servidorSocket.accept(); 
+		
+		InputStreamReader inputReader = new InputStreamReader(ss.getInputStream());
+		BufferedReader reader = new BufferedReader(inputReader);
+		System.out.println(reader.toString());
+		
+		//SolicitarCadastro();
+		
+				
+	}
+	
+	public static void SolicitarCadastro() {
 		
 		
 		try 
-		{	System.out.println("programa 1: servidor");
-		
-			ServerSocket servidorSocket = new ServerSocket(8787,5);
-			Socket ss=servidorSocket.accept();  
+		{	
 			System.out.println("conectado");
 			
-			CrudCliente bd = new CrudCliente();
-			
-			 bd.Conectar();
-			 bd.Ler(3);
+			Crud bd = new Crud();
+			bd.Conectar();
 			 
-			
+			Socket ss=servidorSocket.accept(); 
 			InputStreamReader inputReader = new InputStreamReader(ss.getInputStream());
 			BufferedReader reader = new BufferedReader(inputReader);
 			String x; 
-	
+			System.out.println(reader.toString());
 			
 			ObjectMapper mapper = new ObjectMapper();  
 			
-			Map<String, Object> userData = mapper.readValue(  
-					 reader, new TypeReference<Map<String, Object>>() {  
-	           }); 
-			
-			
-			 String email =  userData.get("Email:").toString();  
-			 String senha =  userData.get("Senha:").toString();  
-			 String nome = userData.get("nome:").toString(); 
-			
-			 bd.Cadastrar(email, nome, senha);
-			
+			Map<String, Object> userData = mapper.readValue(reader, new TypeReference<Map<String, Object>>() {}); 
+						
+			 bd.Cadastrar(userData.get("Email:").toString(),userData.get("nome:").toString(), userData.get("Senha:").toString());
+ 
+			}
+			catch(Exception ex)
+			{
+				System.out.println(ex);
+					
+			}
+	}
+	
+	public static void SolicitarLeitura() {
 		
-	            
-			 
-		}
-		catch(Exception ex)
-		{
-			System.out.println(ex);
-				
-		}
-				
 	}
 
 }
